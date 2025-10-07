60903e0be650d2ee6339db8127fd4c020ced4842
luisxxx1 <luisxxx103@gmail.com>
2025-10-06 19:43:15 -0500

    Primer commit para conectar con repositorio remoto

---
+++ database/migrations/2025_10_05_161318_create_empleados_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id(); 
+
+            $table->String('Nombres');
+            $table->String('Apellidos');
+            $table->String('Documento');
+            $table->text('Correo');
+            $table->String('Rol');
+            $table->integer('edad');
+            $table->String('Foto') -> default('default.jpg');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\http\Controllers\EmpleadoController;
+
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+/*
+Route::get('/empleado', function () {
+    return view('empleado.index');
+});
+
+route::get('empleado/create',[EmpleadoController::class,'create']);
+*/
+
+route::resource('empleado', EmpleadoController::class)-> middleware('auth');
+Auth::routes(['register'=>false,'reset'=>false]);
+
+Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'],  function () {
+    Route::get('/', [EmpleadoController::class, 'index'])->name('home');
+});
+
