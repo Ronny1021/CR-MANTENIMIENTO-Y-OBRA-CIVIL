a6586697c4ffae665d866e0c9791e6e5b7d4c86c
luisxxx1 <luisxxx103@gmail.com>
2025-10-28 23:50:38 -0500

    documentacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,43 +2,47 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <!-- Configuración básica del documento -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
+    <!-- Seguridad para formularios -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.CR', 'CR') }}</title>
+    <!-- Título dinámico desde config/app.php -->
+    <title>{{ config('app.name', 'CR') }}</title>
 
-    <!-- Fonts -->
+    <!-- Fuente externa -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-
-    <!-- Scripts -->
+    <!-- Estilos y scripts compilados con Vite -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
     <div id="app">
+        <!-- Barra de navegación principal -->
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-
+                <!-- Logo o nombre del sistema -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.CR', 'CR') }}
+                    {{ config('app.name', 'CR') }}
                 </a>
 
+                <!-- Botón para menú responsive -->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
+                <!-- Contenido del menú -->
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <!-- Menú izquierdo: enlaces principales -->
+                    <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('empleado.index') }}">{{ __('Empleados') }}</a>
+                            <a class="nav-link" href="{{ route('empleado.index') }}">Empleados</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('/inventario') }}">Inventario</a>
@@ -51,22 +55,22 @@
                         </li>
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
+                    <!-- Menú derecho: autenticación -->
                     <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">Login</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
                                 </li>
                             @endif
                         @else
+                            <!-- Usuario autenticado -->
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -74,10 +78,11 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <!-- Cierre de sesión -->
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        Logout
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -91,6 +96,7 @@
             </div>
         </nav>
 
+        <!-- Área principal donde se carga el contenido de cada vista -->
         <main class="py-4">
             @yield('content')
         </main>
