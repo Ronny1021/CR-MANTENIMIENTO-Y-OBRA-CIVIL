bb9f0ad193bee54c1ecef1f644d3ef316789b9b0
luisxxx1 <luisxxx103@gmail.com>
2025-11-06 19:26:57 -0500

    ULTIMOS CAMBIOS EN CODIGO ANTERIOR PARA MIGRAR AL NUEVO CODIGO

--- resources/views/inventario/index.blade.php
+++ resources/views/inventario/index.blade.php
@@ -7,7 +7,7 @@
     @if(Session::has('mensaje'))
         <div class="alert alert-success" role="alert">
             {{ Session::get('mensaje') }}
-        </div>
+        </div> 
     @endif
 
     {{-- Filtro por disponibilidad --}}
